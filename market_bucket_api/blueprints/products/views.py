5e202a37f737cda9a0d8f84e50cc7566870183d7
Ahmed Ramzi <ahmedramzy160@yahoo.com>
2019-01-16 21:58:58 +0800

    built the hashing tool required to create shopee signatures and successfully fetched products

--- market_bucket_api/blueprints/products/views.py
+++ market_bucket_api/blueprints/products/views.py
@@ -1,12 +1,16 @@
 from flask import jsonify, Blueprint, request, make_response
 import simplejson as json
-from market_bucket import User, Product, Marketplace, db, LAZADA_MARKET_KEY, LAZADA_MARKET_SECRET
+from market_bucket import User, Product, Marketplace, db, LAZADA_MARKET_KEY, LAZADA_MARKET_SECRET, SHOPEE_APP_ID, SHOPEE_APP_KEY
 # from market_bucket.helpers.sendgrid import send_bid_email
 from market_bucket.helpers.lazada_sdk.lazop.base import LazopClient, LazopRequest, LazopResponse
+import time
+import requests
+import hmac
+import hashlib
 
 products_api_blueprint = Blueprint('products_api',
-                               __name__,
-                               template_folder='templates')
+                                   __name__,
+                                   template_folder='templates')
 
 
 @products_api_blueprint.route('/lazada', methods=['GET'])
@@ -26,10 +30,13 @@ def lazada_products():
     user = User.query.get(user_id)
 
     if user:
-        access_token = Marketplace.query.filter_by(user_id = user_id, marketplace_name='lazada').first().access_token
-        refresh_token = Marketplace.query.filter_by(user_id = user_id, marketplace_name='lazada').first().refresh_token
-        client = LazopClient('https://api.lazada.com.my/rest', LAZADA_MARKET_KEY, LAZADA_MARKET_SECRET)
-        laz_request = LazopRequest('/products/get','GET')
+        access_token = Marketplace.query.filter_by(
+            user_id=user_id, marketplace_name='lazada').first().access_token
+        refresh_token = Marketplace.query.filter_by(
+            user_id=user_id, marketplace_name='lazada').first().refresh_token
+        client = LazopClient('https://api.lazada.com.my/rest',
+                             LAZADA_MARKET_KEY, LAZADA_MARKET_SECRET)
+        laz_request = LazopRequest('/products/get', 'GET')
         laz_request.add_api_param('filter', 'live')
         laz_request.add_api_param('limit', '10')
         laz_request.add_api_param('options', '1')
@@ -42,7 +49,7 @@ def lazada_products():
             'products': products
         }
 
-        return make_response(jsonify(responseObject)), 201
+        return make_response(jsonify(responseObject)), 200
 
     else:
         responseObject = {
@@ -52,100 +59,64 @@ def lazada_products():
 
         return make_response(jsonify(responseObject)), 401
 
-# @products_api_blueprint.route('/', methods=['GET'])
-# def index():
-#     bids = Bid.query.all()
-#     all_bids = []
-#     for bid in bids:
-#         del bid.__dict__['_sa_instance_state']
-#         all_bids.append(bid.__dict__)
 
-#     return json.dumps(all_bids)
+@products_api_blueprint.route('/shopee', methods=['GET'])
+def shopee_products():
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'No authorization header found'
+        }
 
+        return make_response(jsonify(responseObject)), 401
 
-# @products_api_blueprint.route('/new_bid', methods=['POST'])
-# def new_bid():
-#     # get the post data
-#     post_data = request.get_json()
-#     user_id = post_data.get('user_id')
-#     amount = post_data.get('amount')
-#     nonce_from_the_client = post_data.get('nonce')
-#     result = gateway.transaction.sale({
-#         "amount": amount,
-#         "payment_method_nonce": nonce_from_the_client,
-#         "options": {
-#             "submit_for_settlement": False
-#         }
-#     })
+    user_id = User.decode_auth_token(auth_token)
+    user = User.query.get(user_id)
 
-#     if result.is_success:
-#         new_bid = Bid(
-#             user_id=user_id,
-#             billboard_id=post_data.get('billboard_id'),
-#             medium_id=post_data.get('medium_id'),
-#             booking_at=post_data.get('booking_at'),
-#             amount=amount
-#         )
-#         db.session.add(new_bid)
-#         db.session.commit()
-#         send_bid_email(User.query.get(user_id).email, new_bid.id)
+    if user:
+        shop_id = Marketplace.query.filter_by(
+            user_id=user_id, marketplace_name='shopee').first().shop_id
+        endpoint = "https://partner.shopeemobile.com/api/v1/items/get"
+        request_body = json.dumps({'shopid': shop_id, 'partner_id': int(SHOPEE_APP_ID), 'timestamp': int(
+            time.time()), 'pagination_offset': 0, 'pagination_entries_per_page': 100})
+        signature = hmac.new(
+            key=bytes(SHOPEE_APP_KEY, encoding="ascii"),
+            msg=bytes(f'{endpoint}|{request_body}', encoding="ascii"),
+            digestmod=hashlib.sha256).hexdigest()
+        headers = {'Authorization': signature}
+        response = requests.post(endpoint, headers=headers, data=request_body)
 
-#         responseObject = {
-#             'status': 'success',
-#             'message': result.transaction.status,
-#             'details': f'{result.transaction.processor_response_code} : {result.transaction.processor_response_text}'
-#         }
+        products = []
+        for item in response.json()['items']:
+            item_id = item['item_id']
+            endpoint = "https://partner.shopeemobile.com/api/v1/item/get"
+            request_body = json.dumps({'shopid': shop_id, 'item_id': item_id, 'partner_id': int(SHOPEE_APP_ID), 'timestamp': int(
+                time.time())})
+            signature = hmac.new(
+                key=bytes(SHOPEE_APP_KEY, encoding="ascii"),
+                msg=bytes(f'{endpoint}|{request_body}', encoding="ascii"),
+                digestmod=hashlib.sha256).hexdigest()
+            headers = {'Authorization': signature}
+            response = requests.post(
+                endpoint, headers=headers, data=request_body)
+            products.append(response.json()['item'])
 
-#         return make_response(jsonify(responseObject)), 201
+        breakpoint()
+        responseObject = {
+            'status': 'success',
+            'message': 'all products returned',
+            'products': products
+        }
 
-#     else:
-#         responseObject = {
-#             'status': ' api success but payment failed',
-#             'message': result.transaction.status,
-#             'details': f'{result.transaction.processor_response_code} : {result.transaction.processor_response_text}'
-#         }
+        return make_response(jsonify(responseObject)), 200
 
-#         return make_response(jsonify(responseObject)), 201
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'Authentication failed'
+        }
 
-
-# @products_api_blueprint.route('/me', methods=['GET'])
-# def show():
-#     auth_header = request.headers.get('Authorization')
-
-#     if auth_header:
-#         auth_token = auth_header.split(" ")[1]
-#     else:
-#         responseObject = {
-#             'status': 'failed',
-#             'message': 'No authorization header found'
-#         }
-
-#         return make_response(jsonify(responseObject)), 401
-
-#     user_id = User.decode_auth_token(auth_token)
-#     user = User.query.get(user_id)
-
-#     if user:
-#         bids = user.bids
-#         all_bids = []
-#         for bid in bids:
-#             del bid.__dict__['_sa_instance_state']
-#             bid.__dict__['created_at_readable'] = bid.created_at_readable
-#             bid.__dict__['booking_at_readable'] = bid.booking_at_readable
-#             all_bids.append(bid.__dict__)
-
-#         responseObject = {
-#             'status': 'success',
-#             'message': 'All bids for user returned',
-#             'all_ads': all_bids
-#         }
-
-#         return make_response(json.dumps(responseObject)), 201
-
-#     else:
-#         responseObject = {
-#             'status': 'failed',
-#             'message': 'Authentication failed'
-#         }
-
-#         return make_response(jsonify(responseObject)), 401
+        return make_response(jsonify(responseObject)), 401
