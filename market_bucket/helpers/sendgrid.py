ac10895cac600991f0b079ee91499364d8c6dff6
Ahmed Ramzi <ahmedramzy160@yahoo.com>
2019-01-19 07:13:49 +0800

    added support for shopee's cat_tree, attributes and creating products

--- market_bucket/helpers/sendgrid.py
+++ market_bucket/helpers/sendgrid.py
@@ -4,22 +4,6 @@ import time
 import datetime
 
 
-# def send_product_email(to, bid_id):
-#     bid = Bid.query.get(bid_id)
-#     billboard = Billboard.query.get(bid.billboard_id)
-#     from_email = Email("bids@bidboard.com")
-#     to_email = Email(to)
-#     subject = f"Thank You For Placing Your Bid {User.query.get((bid.user_id)).first_name}!"
-#     content = Content(
-#         "text/html", f"<html><table><tr><td align='center'><img src='https://s3.amazonaws.com/bidboard/11.31851.logo.jpg' height='200px'></td></tr><tr><td>Dear {User.query.get((bid.user_id)).first_name} {User.query.get((bid.user_id)).last_name},<br><br>You have just placed a {bid.amount} MYR bid on BidBoard. The details of your booking are as follows: <br> <br>Billboard size: {billboard.size} <br>Billboard location: {billboard.location} <br>Booking Date and time: {bid.booking_at_readable}<br> <br> This is <b>NOT</b> a confirmed booking. You will recieve another email when the bidding closes to confirm your booking if you are the highest bidder. You will also recieve an email to notify you if someone has outbidded you. <br> Happy Advertising! <br><br>Kind regards, <br> Bidboard Team </td></tr><tr><td align='center'>This is a no-reply address. If you have any questions, please email us <a href='mailto:ahmed160ramzi@gmail.com'>here.</a></td></tr></table>"
-#     )
-
-#     mail = Mail(from_email, subject, to_email, content)
-#     response = sg.client.mail.send.post(request_body=mail.get())
-# print(response.status_code)
-# print(response.body)
-# print(response.headers)
-
 def send_signup_email(to, user_id):
     from_email = Email("signup@marketbucket.com")
     to_email = Email(to)
@@ -38,6 +22,18 @@ def send_new_marketplace_email(to, user_id, marketplace):
     subject = f"{marketplace} added to MarketBucket {User.query.get(user_id).first_name}!"
     content = Content(
         "text/html", f"<html><table><tr><td align='center'><img src='https://s3.amazonaws.com/market.bucket/1.11556.Screenshot_2019-01-08_at_21.01.12.jpg' height='200px'></td></tr><tr><td>Dear {User.query.get((user_id)).first_name} {User.query.get((user_id)).last_name},<br><br>You have just added {marketplace} to your MarketBucket! <br>You can now add products to all your online stores through MarketBucket and relax while we take care of your stocks and products for you. We encourage you to add your other marketplaces with us if you haven't already in order to maximise the potential of what we can do for you. <br> We wish you the most successful of experiences with MarketBucket! <br> <br> Kind regards, <br> MarketBucket Team </td></tr><tr><td align='center'>This is a no-reply address. If you have any questions, please email us <a href='mailto:ahmed160ramzi@gmail.com'>here.</a></td></tr></table>"
+    )
+
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+
+
+def send_new_product_email(to, user_id, name):
+    from_email = Email("products@marketbucket.com")
+    to_email = Email(to)
+    subject = f"{name} added to MarketBucket {User.query.get(user_id).first_name}!"
+    content = Content(
+        "text/html", f"<html><table><tr><td align='center'><img src='https://s3.amazonaws.com/market.bucket/1.11556.Screenshot_2019-01-08_at_21.01.12.jpg' height='200px'></td></tr><tr><td>Dear {User.query.get((user_id)).first_name} {User.query.get((user_id)).last_name},<br><br>You have just added {name} to your MarketBucket! <br>MarketBucket will publish your product according to your preferences and take care of your stock management for you. We encourage you to add more products and marketplaces with us in order to maximise the potential of what we can do for you. <br> We wish you the most successful of experiences with MarketBucket! <br> <br> Kind regards, <br> MarketBucket Team </td></tr><tr><td align='center'>This is a no-reply address. If you have any questions, please email us <a href='mailto:ahmed160ramzi@gmail.com'>here.</a></td></tr></table>"
     )
 
     mail = Mail(from_email, subject, to_email, content)
--- market_bucket_api/blueprints/products/views.py
+++ market_bucket_api/blueprints/products/views.py
@@ -7,7 +7,7 @@ import time
 import requests
 import hmac
 import hashlib
-import xml.etree.ElementTree as ET
+from market_bucket.helpers.sendgrid import send_new_product_email
 
 
 products_api_blueprint = Blueprint('products_api',
@@ -149,17 +149,17 @@ def lazada_category_tree():
         laz_request = LazopRequest('/category/tree/get', 'GET')
         response = client.execute(laz_request)
         tree = response.body['data']
-        laz_request2 = LazopRequest('/brands/get', 'GET')
-        laz_request2.add_api_param('offset', '0')
-        laz_request2.add_api_param('limit', '1000')
-        response2 = client.execute(laz_request2)
-        brands = response2.body['data']
+        # laz_request2 = LazopRequest('/brands/get', 'GET')
+        # laz_request2.add_api_param('offset', '0')
+        # laz_request2.add_api_param('limit', '1000')
+        # response2 = client.execute(laz_request2)
+        # brands = response2.body['data']
 
         responseObject = {
             'status': 'success',
             'message': 'category tree returned',
             'tree': tree,
-            'brands': brands
+            # 'brands': brands
         }
 
         return make_response(jsonify(responseObject)), 200
@@ -223,14 +223,186 @@ def lazada_new_product():
         laz_request = LazopRequest('/product/create')
         laz_request.add_api_param('payload', f"<?xml version=\"1.0\" encoding=\"UTF-8\" ?> <Request>     <Product>         <PrimaryCategory>{category_id}</PrimaryCategory>         <SPUId></SPUId>         <AssociatedSku></AssociatedSku>         <Attributes>             <name>{name}</name>             <short_description>{description}</short_description>             <brand>{brand}</brand>             <model>no model</model>         </Attributes>         <Skus>             <Sku>                 <SellerSku>MarketBucket-{name}</SellerSku>                 <color_family>{color}</color_family>                 <images><image>https://steamcdn-a.akamaihd.net/steam/apps/470220/header.jpg?t=1507630376</image></images>                                  <quantity>{quantity}</quantity>                 <price>{price}</price>                 <package_length>{length}</package_length>                 <package_height>{height}</package_height>                 <package_weight>{weight}</package_weight>                 <package_width>{width}</package_width>                 <package_content>{content}</package_content>                 <tax_class>default</tax_class>                          </Sku>         </Skus>     </Product> </Request>")
         response = client.execute(laz_request, access_token)
+        code = response.code
+
+        if code == 0:
+            send_new_product_email(user.email, user_id, name)
+            responseObject = {
+                'status': 'success',
+                'message': response.body,
+            }
+        else:
+            responseObject = {
+                'status': 'failed',
+                'message': response.body,
+            }
+
+        breakpoint()
+        return make_response(jsonify(responseObject)), 200
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'Authentication failed'
+        }
+
+        return make_response(jsonify(responseObject)), 401
+
+
+@products_api_blueprint.route('/shopee/tree', methods=['GET'])
+def shopee_category_tree():
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'No authorization header found'
+        }
+
+        return make_response(jsonify(responseObject)), 401
+
+    user_id = User.decode_auth_token(auth_token)
+    user = User.query.get(user_id)
+
+    if user:
+        shop_id = Marketplace.query.filter_by(
+            user_id=user_id, marketplace_name='shopee').first().shop_id
+        endpoint = "https://partner.shopeemobile.com/api/v1/item/categories/get"
+        request_body = json.dumps({"shopid": shop_id, "partner_id": int(SHOPEE_APP_ID), "timestamp": int(
+            time.time())})
+        signature = hmac.new(
+            key=bytes(SHOPEE_APP_KEY, encoding="ascii"),
+            msg=bytes(f'{endpoint}|{request_body}', encoding="ascii"),
+            digestmod=hashlib.sha256).hexdigest()
+        headers = {'Authorization': signature}
+        response = requests.post(endpoint, headers=headers, data=request_body)
 
         responseObject = {
             'status': 'success',
-            'message': 'product added successfully',
+            'message': 'category tree returned',
+            'tree': response.json()['categories'],
         }
-        breakpoint()
+
         return make_response(jsonify(responseObject)), 200
 
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'Authentication failed'
+        }
+
+        return make_response(jsonify(responseObject)), 401
+
+
+@products_api_blueprint.route('/shopee/new', methods=['POST'])
+def shopee_new_product():
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'No authorization header found'
+        }
+
+        return make_response(jsonify(responseObject)), 401
+
+    user_id = User.decode_auth_token(auth_token)
+    user = User.query.get(user_id)
+
+    if user:
+        post_data = request.get_json()
+        category_id = post_data.get('id')
+        name = post_data.get('name')
+        description = post_data.get('description')
+        price = post_data.get('price')
+        quantity = post_data.get('quantity')
+        weight = post_data.get('package_weight')
+        attribute1 = post_data.get('attribute1')
+        attribute2 = post_data.get('attribute2')
+        attribute3 = post_data.get('attribute3')
+        if attribute2 == {}:
+            attributes = [attribute1]
+        elif attribute3 == {}:
+            attributes = [attribute1, attribute2]
+        else:
+            attributes = [attribute1, attribute2, attribute3]
+
+        shop_id = Marketplace.query.filter_by(
+            user_id=user_id, marketplace_name='shopee').first().shop_id
+        endpoint = "https://partner.shopeemobile.com/api/v1/item/add"
+        request_body = json.dumps({"shopid": shop_id, "category_id": category_id, 'name': name, 'description': description, 'price': int(price), 'stock': int(quantity), 'images': [{'url': 'https://cdn1.ebags.com/is/image/im3/334833_1_1?resmode=4&op_usm=1,1,1,&qlt=70,1&hei=600&wid=600&align=0,1'}], "attributes": attributes, "logistics": [{"logistic_id": 29210, "enabled": True}], "weight": int(weight), "partner_id": int(SHOPEE_APP_ID), "timestamp": int(
+            time.time())})
+        signature = hmac.new(
+            key=bytes(SHOPEE_APP_KEY, encoding="ascii"),
+            msg=bytes(f'{endpoint}|{request_body}', encoding="ascii"),
+            digestmod=hashlib.sha256).hexdigest()
+        headers = {'Authorization': signature}
+        response = requests.post(endpoint, headers=headers, data=request_body)
+        breakpoint()
+
+        try:
+            response.json()['item_id']
+        except:
+            send_new_product_email(user.email, user_id, name)
+            responseObject = {
+                'status': 'success',
+                'message': response.json(),
+            }
+        else:
+            responseObject = {
+                'status': 'failed',
+                'message': response.json(),
+            }
+
+        return make_response(jsonify(responseObject)), 200
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'Authentication failed'
+        }
+
+        return make_response(jsonify(responseObject)), 401
+
+
+@products_api_blueprint.route('/shopee/attributes', methods=['POST'])
+def shopee_attributes():
+    auth_header = request.headers.get('Authorization')
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'No authorization header found'
+        }
+
+        return make_response(jsonify(responseObject)), 401
+
+    user_id = User.decode_auth_token(auth_token)
+    user = User.query.get(user_id)
+
+    if user:
+        post_data = request.get_json()
+        category_id = post_data.get('id')
+        shop_id = Marketplace.query.filter_by(
+            user_id=user_id, marketplace_name='shopee').first().shop_id
+        endpoint = "https://partner.shopeemobile.com/api/v1/item/attributes/get"
+        request_body = json.dumps({"shopid": shop_id, "partner_id": int(SHOPEE_APP_ID), "timestamp": int(
+            time.time()), "category_id": category_id})
+        signature = hmac.new(
+            key=bytes(SHOPEE_APP_KEY, encoding="ascii"),
+            msg=bytes(f'{endpoint}|{request_body}', encoding="ascii"),
+            digestmod=hashlib.sha256).hexdigest()
+        headers = {'Authorization': signature}
+        response = requests.post(endpoint, headers=headers, data=request_body)
+
+        responseObject = {
+            'status': 'success',
+            'message': 'attributes for the selected category returned',
+            'attributes': response.json()['attributes']
+        }
+
+        return make_response(jsonify(responseObject)), 200
     else:
         responseObject = {
             'status': 'failed',
